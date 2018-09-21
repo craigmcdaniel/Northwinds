1f4b231fe59bfc52e0c40b0e9b36a1a7721580a6
craigmcdaniel <craigmcdaniel@users.noreply.github.com>
2018-09-21 13:00:44 -0400

    Update modal issues

--- src/app/product-details-modal/product-details-modal.component.ts
+++ src/app/product-details-modal/product-details-modal.component.ts
@@ -1,33 +1,33 @@
 
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbActiveModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-product-details-modal',
-  template: `
-    <div ngbActiveModal class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-    </div>
-  `
+  templateUrl:'product-details-modal.component.html'
 })
 export class ProductDetailsModalComponent {
-  constructor(private modalService: NgbModal) {}
-  @Input() name;
-  open() {
-    const modalRef = this.modalService.open(ProductDetailsModalComponent);
-    modalRef.componentInstance.name = 'World';
-  }
- 
-}
+  closeResult: string;
 
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content,).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
 
