14928f6785eb36751b55adc01163809719517ce8
craigmcdaniel <craigmcdaniel@users.noreply.github.com>
2018-10-21 05:22:18 -0400

    Revert "Merge branch 'master' of https://github.com/craigmcdaniel/Northwinds"
    This reverts commit c55543153bb86ba1f3f5308be123c00c5f15bb3f, reversing
    changes made to 6f9b76db6814507425b471a70ee4a14d3bb5643e.

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -7,15 +7,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AdminService {
   
-  
+  private AdminUrl = 'http://localhost:3001/admin';
+
   constructor(
     
-  
+    private authService: AuthService
   ) { }
 
  
 
   // Implement a method to get the private deals
- 
+  getAdmin() {
+    return this.http
+      .get(this.AdminUrl, {
+        headers: new HttpHeaders().set('Authorization', `Bearer ${this.authService.accessToken}`)
+      });
+      
+  }
  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './auth/auth.service';
 import { CommonModule } from '@angular/common';  
 import { BrowserModule } from '@angular/platform-browser';
-import * as firebase from 'firebase';
-
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
 
 
 
@@ -14,21 +10,10 @@ import { Observable } from 'rxjs/Observable';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[AuthService]
 })
 export class AppComponent {
   title = 'app';
- 
-  public user: Observable<firebase.User>;
 
-
-  constructor(public auth: AuthService, public afAuth:AngularFireAuth) {
- 
-
- 
-
-
-  }
- 
+  constructor(public auth: AuthService) {}
   
 }
